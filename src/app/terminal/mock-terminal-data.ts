861a4c0ad55798065b63d34c455234e45b07828a
Tom H <tom@codersforcorbyn.com>
2017-05-06 19:07:34 +0100

    srs policy debate

--- src/app/terminal/mock-terminal-data.ts
+++ src/app/terminal/mock-terminal-data.ts
@@ -2,5 +2,8 @@ import { TerminalLine } from './terminal-line';
 
 export const DATA: TerminalLine[] = [
   { text: "Booting System...", type: "info", pause: 0.1 },
-  { text: "Running Diagnostic Checks", type: "info", pause: 2 }
+  { text: "Running Diagnostic Checks", type: "info", pause: 2 },
+  { text: "Economy Module: Unstable", type: "info", pause: 2 },
+  { text: "Security Module: Compromised", type: "info", pause: 2 },
+  { text: "ALERT: System Failure Imminent", type: "info", pause: 2}
 ]
