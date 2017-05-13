f4367b1604174dbb1938458455113b0cabe5dc4c
Tom H <tom@codersforcorbyn.com>
2017-05-13 11:25:25 +0100

    Added volunteer notice and altered text

--- src/app/terminal/mock-terminal-data.ts
+++ src/app/terminal/mock-terminal-data.ts
@@ -6,13 +6,14 @@ export const DATA: TerminalLine[] = [
   { text: "Premature Reboot Detected: Analysing", type: "info", pause: 0 , active: false},
   { text: "System shut down unexpectedly - work in progress may have been lost.", type: "info", pause: 2, active: false},
   { text: "Inspecting Cache", type: "info", pause: 1 , active: false},
-  { text: "1 File Detected: 'Brexit Plans.doc'", type: "info", pause: 3 , active: false},
+  { text: "1 File Reference Detected: 'Brexit Plans.doc'", type: "info", pause: 3 , active: false},
   { text: "Attempting Recovery of 'Brexit Plans.doc'", type: "info", pause: 1 , active: false},
-  { text: "Cannot Recover 'Brexit Plans.doc'", type: "info", pause: 1.5 , active: false},
-  { text: "Reason: File Corrupted", type: "alert", pause: 0.25 , active: false},
+  { text: "Unable to recover 'Brexit Plans.doc' | File Does Not Exist", type: "alert", pause: 1.5 , active: false},
   { text: "Running Diagnostic Checks", type: "info", pause: 2, active: false },
-  { text: "Economy Module: Unstable", type: "info", pause: 2, active: false},
-  { text: "Security Module: Compromised", type: "info", pause: 2, active: false },
+  { text: "Economy Module (LTEP): Unstable - Resource allocation is limited to 1% although more is available.", type: "info", pause: 2, active: false},
+  { text: "System Healthcheck Module: Error - NHS.svc fragmented", type: "info", pause: 2, active: false},
+  { text: "Security Module: Compromised - Unable to start up new security workers", type: "info", pause: 2, active: false },
+  { text: "Malware Detected: t0ri35.bin", type: "alert", pause: 4, active: false },
   { text: "ALERT: System Failure Imminent", type: "alert", pause: 2, active: false },
-  { text: "A System Upgrade is Available: Downloading", type: "alert", pause: 2, active: false }
+  { text: "Gov.OS 2017 is Available: Upgrading", type: "info", pause: 2, active: false }
 ]
