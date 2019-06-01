c6bc4320533ef380bde0fbb9678bf55752b5571e
Tom <tom@codersforcorbyn.com>
2019-06-01 18:47:32 +0100

    Angular upgrade
    Upgrading Angular and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
@@ -14,7 +14,7 @@ import { TerminalComponent } from './terminal/terminal.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
